95829fff3906877074223d1dcbccf6d221556cd9
s79082 <s79082@htw-dresden.de>
2023-08-19 19:58:40 +0200

    mods

--- main.go
+++ main.go
@@ -1,50 +1,58 @@
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "os"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
+
 func enableCors(w *http.ResponseWriter) {
-        (*w).Header().Set("Access-Control-Allow-Origin", "*")
-        
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+
 }
 
 func main() {
 
+	conn()
 
-        conn()
+	// Define the port number
+	const port = 80
 
-        // Define the port number
-        const port = 80
-
-        fs := http.FileServer(http.Dir("./static"))
+	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-        // Setup a basic endpoint
-        http.HandleFunc("/api/playlist", func(w http.ResponseWriter, r *http.Request) {
-                enableCors(&w)
+	// Setup a basic endpoint
+	http.HandleFunc("/api/playlist", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
 
-                if r.Method == http.MethodGet {
-                        return;
+		if r.Method == http.MethodGet {
+			return
 
-                }
+		}
 
-                fmt.Fprint(w, "Hello, world!")
-        })
+		fmt.Fprint(w, "Hello, world!")
+	})
+	http.HandleFunc("/api/msg", func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
 
-        // Start the HTTP server
-        address := fmt.Sprintf(":%d", port)
-        log.Printf("Starting HTTP server on %s", address)
-        err := http.ListenAndServe(address, nil)
-        if err != nil {
-                os.Exit(3)
-                log.Fatalf("Error starting server: %v", err)
-        }
-        
+		d, _ := io.ReadAll(r.Body)
 
-        println("hewwlo")
+		log.Printf("new msg: %s", string(d))
+
+		fmt.Fprint(w, "Hello, world!")
+	})
+
+	// Start the HTTP server
+	address := fmt.Sprintf(":%d", port)
+	log.Printf("Starting HTTP server on %s", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		os.Exit(3)
+		log.Fatalf("Error starting server: %v", err)
+	}
+
+	println("hewwlo")
 
 }
-
